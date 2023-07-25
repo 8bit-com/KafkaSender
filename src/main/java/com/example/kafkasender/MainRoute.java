9cafea2ac30b6f0de6d811da7e5ea842f0ea8f5b
Vladimir <v.dementev@bftcom.com>
2023-07-25 22:12:53 +0700

    fix

--- src/main/java/com/example/kafkasender/MainRoute.java
+++ src/main/java/com/example/kafkasender/MainRoute.java
@@ -12,19 +12,18 @@ public class MainRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        //from("timer://" + "name" + "?repeatCount=2")
-        from("timer://" + "name" + "?fixedRate=true&period=1&repeatCount=2")
+        from("timer://" + "name" + "?fixedRate=true&period=1&repeatCount=1")
                 .process(exchange -> {
                     MessageDto messageDto = new MessageDto();
                     messageDto.setTo("eyJzaWQiOjYxMTM4OCwibWlkIjoiODg1ODM1YWMtZmUwMC0xMWVjLTg0MTgtNTI1NDAwMDFhYzMyIiwidGNkIjoiNGQ2ZmNkZTItY2IyNC00YjE5LWFjMDktODkyNGFmMThiN2E0IiwiZW9sIjowLCJzbGMiOiJpaXMuZWNwLnJ1X3pucF8xLjAuMF9SZXF1ZXN0IiwibW5tIjoiVTQ4MjkwMSIsIm5zIjoidXJuOi8vaWlzLmVjcC5ydS96bnAvMS4wLjAifQ==");
                     messageDto.setDtoMessage("ru.gov.pfr.ecp.rostrud.test.root.TestRostrudUchetRequestFromScmzToWso");
                     messageDto.setMessageId(UuidCreator.getTimeBased().toString());
                     messageDto.setTaskId(id);
-                    messageDto.setDaEntityId("7c1f9b25-5737-4b7d-a151-b488807c0500");
-                    messageDto.setSendXmlGuid("5886b447-4093-417d-bf8e-9be2bba1ace9");
-                    if (id==8L)
-                        id = 7L;
-                    else id = 8L;
+                    messageDto.setDaEntityId("135fbddc-2ace-11ee-be56-0242ac120002");
+                    messageDto.setSendXmlGuid("934561d4-fb4d-49e9-a453-66ee74a3cc2d");
+//                    if (id==8L)
+//                        id = 7L;
+//                    else id = 8L;
                     exchange.getIn().setBody(messageDto);
                 })
                 .marshal().json(JsonLibrary.Jackson, MessageDto.class)
